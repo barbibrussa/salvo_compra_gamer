225cc308695daf529a0140e856f83ca152c24d63
barbara <barbibrussa@gmail.com>
2021-11-01 20:15:05 -0300

    Creación de modelo Game y su controlador, incluye patrón de repositorio

---
+++ Salvo/Salvo/Controllers/GamesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Salvo.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Salvo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private IGameRepository _repository;
+        public GamesController(IGameRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/<GamesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try 
+            {
+                var games = _repository.GetAllGames();
+                return Ok(games);
+            } 
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // GET api/<GamesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GamesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GamesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GamesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Salvo/Salvo/Startup.cs
+++ Salvo/Salvo/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Salvo.Models;
+using Salvo.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace Salvo
         {
             services.AddRazorPages();
             services.AddDbContext<SalvoContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("SalvoDataBase")));
+            services.AddScoped<IGameRepository, GameRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +52,9 @@ namespace Salvo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=games}/{ action = Get}");
             });
         }
     }
